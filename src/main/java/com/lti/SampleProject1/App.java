55d0ebedd614604ecc8498edf93c4c33c9f0e86a
ridzz1599 <ridzz1599@gmail.com>
2020-11-21 16:43:38 +0530

    initial commit

---
+++ src/main/java/com/lti/SampleProject1/App.java
@@ -0,0 +1,16 @@
+package com.lti.SampleProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public void m1(){
+    	System.out.print("Inside m1");
+    }
+}
